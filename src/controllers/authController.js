5f452afac3911531564b68f2c202005ef2466d6f
MrSeryLol <psygentelmen014@gmail.com>
2023-05-18 13:20:33 +0300

    Update registration form and create login form

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,16 @@
 const ErrorAPI = require('../error/errorAPI')
 const { User, Role, Company, sequelize } = require('../models')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const generateAccessToken = (id, role) => {
+    const payload = {
+        id, 
+        role
+    }
+    return jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: '24h'})
+}
+
 
 class AuthController {
     async registration(req, res, next) {
@@ -42,17 +52,54 @@ class AuthController {
                     }
                 }, { transaction: transaction });
 
-                user.addRole(user.id, role.id), { transaction: transaction };
-            return res.json({ user })
-        })
-    } catch(err) {
-        console.log(err)
-        //next(ErrorAPI.badRequest(err.message))
+                await user.addRole(role.id, { transaction: transaction });
+                return res.json({ user })
+            })
+        } catch (err) {
+            //console.log(err)
+            next(ErrorAPI.badRequest(err.message))
+        }
     }
-}
-    async login(req, res) {
+    async login(req, res, next) {
+        try {
+            const { login, password } = req.body
+            const user = await User.findOne({
+                where: {
+                    login: login
+                }
+            })
+            if (!user) {
+                return next(ErrorAPI.badRequest("Пользователь не найден"))
+            }
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if (!validPassword) {
+                return next(ErrorAPI.badRequest("Введён неверный пароль"))
+            }
 
-}
+            const role = await Role.findOne({
+                attributes: ['role'],
+                where: {
+                    role: 'Руководитель'
+                },
+                include: { 
+                    model: User,
+                },
+            })
+            console.log(role)
+
+            const token = generateAccessToken(user.id, role.role)
+
+
+            return res.json({token})
+
+            
+
+
+        } catch(err) {
+            next(ErrorAPI.badRequest(err.message))
+        }
+
+    }
 }
 
 module.exports = new AuthController()
